63368910cfc01da025d8e411f565df67468542c3
DeanBolte <Dean.bolte7@gmail.com>
2020-10-07 09:35:30 +1100

    added IO, added basic functionality

--- app/Main.hs
+++ app/Main.hs
@@ -6,4 +6,15 @@ main :: IO ()
 main = do
     putStrLn "Please enter a Message: "
     message <- getLine
-    multiKey (read message)
+    putStrLn "Enter p: "
+    p <- getLine
+    putStrLn "Enter q: "
+    q <- getLine
+    putStrLn "Enter k1: "
+    k1 <- getLine
+    putStrLn "Enter k2: "
+    k2 <- getLine
+    putStrLn "Enter k3: "
+    k3 <- getLine
+    putStrLn "unencrypted message: "
+    multiKey (read message) (read p) (read q) (read k1) (read k2) (read k3)
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,15 +4,30 @@ module Lib
 
 import Prelude
 
-multiKey :: Int -> IO ()
-multiKey message = invmod 3 26
+multiKey :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> IO ()
+multiKey message p q k1 k2 k3 = do
+    -- Bank
+    let n = p * q
+    let phi = (p - 1) * (q - 1)
+    let k4 = parseMaybeInt $ (k1 * k2 * k3) `invmod` phi
+    print k4
+    -- Alice, Bob, Karen
+    let s = (message ^ (k1 * k3 * k4)) `mod` n
+    print s
+    -- Verification
+    let m' = (s ^ k2) `mod` n
+    print m'
 
-invmod :: Int -> Int -> Maybe Int
+parseMaybeInt :: Maybe Integer -> Integer
+parseMaybeInt (Just x) = x
+parseMaybeInt Nothing = 0
+
+invmod :: Integer -> Integer -> Maybe Integer
 invmod a m = let invmod' (g, x, _) | g /= 1 = Nothing
                                    | otherwise = Just (x `mod` m)
              in invmod' $ egcd a m
 
-egcd :: Int -> Int -> (Int, Int, Int)
+egcd :: Integer -> Integer -> (Integer, Integer, Integer)
 egcd 0 b = (b, 0, 1)
 egcd a b = let out (g, y, x) = (g, x - (b `div` a) * y, y)
            in out $ egcd (b `mod` a) a
